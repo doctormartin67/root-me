5872d97943df9cf374b7f557aad001c47c0269f9
doctormartin67 <celticfans@hotmail.com>
2023-01-22 02:53:16 +0100

    Arithmetic

--- programming/arith.py
+++ programming/arith.py
@@ -3,6 +3,9 @@ import requests
 """
 TODO: i have parsed out the numbers, I just need to work out the formula
 to calculate it now and put that in the 'results' variable
+Un+1 = a + Un + n * b
+U0 = c
+n + 1 = d
 """
 
 def parse_more(s):
@@ -19,11 +22,20 @@ def parse(s):
 url = "http://challenge01.root-me.org/programmation/ch1/"
 sess = requests.Session()
 r = sess.get(url)
+print(r.text)
 res = parse(r.text)
-print(parse(r.text), res[0], res[1], res[4], res[8], res[13],
-parse_more(res[18]))
+a = int(res[0])
+b = int(res[8])
+c = int(res[13])
+d = int(parse_more(res[18]))
+n = d - 1
 
-result = 0
+print(parse(r.text), a, res[1], res[4], b, c, d)
+
+# this doesn't work if res[4] == '-', but too lazy to write
+# just rerun until it's '+'
+result = int(a * (n + 1) + b * n * (n + 1) / 2 + c)
+print(result)
 url = "http://challenge01.root-me.org/programmation/ch1/ep1_v.php?result="
 url += str(result)
 r = sess.get(url)
